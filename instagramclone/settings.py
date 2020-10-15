880dca9795792667f947221dbc79f158131de9ae
omarion3698 <omaribinbakarivic@gmail.com>
2020-10-15 19:58:27 +0300

    Hide secret key in the project

--- instagramclone/settings.py
+++ instagramclone/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tew_xb=xok(t#b#y(4a$2%m$9bq))t%rz9)b=3$pqx7p!5z3i%'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
