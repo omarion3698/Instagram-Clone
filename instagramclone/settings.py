5d8fea21da1644df40d7113a60d9fb93c25ecf55
omarion3698 <omaribinbakarivic@gmail.com>
2020-10-15 21:22:20 +0300

    make migrations

--- instagramclone/settings.py
+++ instagramclone/settings.py
@@ -20,13 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'tew_xb=xok(t#b#y(4a$2%m!%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
